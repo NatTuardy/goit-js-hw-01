8f60f3744da79faa89f955e5de06ba557e679a9d
Your Name <you@example.com>
2020-03-03 17:55:36 +0200

    fixed mistakes

--- js/task-4.js
+++ js/task-4.js
@@ -1,33 +1,20 @@
 'use strict';
 
 const credits = 23580;
-const accauntUser = credits;
 const pricePerDroid = 3000;
-
 const promptLabel = 'Выбери колличество дроидов';
-
-let userChoise = prompt(promptLabel);
-let totalPrice = userChoise;
-let balance = 0;
 let message;
+let userChoise = prompt(promptLabel);
 
 if (userChoise === null) {
     message = 'Отменено пользователем!';
 } else if (userChoise = Number(userChoise)) {
     if (credits < pricePerDroid * userChoise) {
         message = 'Недостаточно средств на счету!';
-        alert(message);
+
     } else if (credits > pricePerDroid * userChoise) {
-        let balance = credits - (userChoise * pricePerDroid);
+        const balance = credits - (userChoise * pricePerDroid);
         message = `'Вы купили ${userChoise} дроидов, на счету осталось ${balance} кредитов.'`;
-        alert(message);
     }
 }
-
-// console.log(totalPrice);
-// console.log(message);
-// const total = 100;
-// const ordered = 50;
-// const result = total >= ordered
-
-// console.log('result);
+alert(message);
