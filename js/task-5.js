f9c455544ffe38b9bdd3a7d146d64595487af2e6
Your Name <you@example.com>
2020-03-03 17:08:54 +0200

    fixed mistake

--- js/task-5.js
+++ js/task-5.js
@@ -9,37 +9,39 @@ switch (country.toLowerCase()) {
     case 'китай':
         price = 100;
         message = `'Доставка в ${country} будет стоить ${price} кредитов'`;
-        alert(message);
+        // alert(message);
         break;
 
     case 'чили':
         price = 250;
         message = `'Доставка в ${country} будет стоить ${price} кредитов'`;
-        alert(message);
+        // alert(message);
         break;
 
     case 'австралия':
         price = 170;
         message = `'Доставка в ${country} будет стоить ${price} кредитов'`;
-        alert(message);
+        // alert(message);
         break;
 
     case 'индия':
         price = 80;
         message = `'Доставка в ${country} будет стоить ${price} кредитов'`;
-        alert(message);
+        // alert(message);
         break;
 
     case 'ямайка':
         price = 120;
         message = `'Доставка в ${country} будет стоить ${price} кредитов'`;
-        alert(message);
+        // alert(message);
         break;
 
     default:
         message = 'В вашей стране доставка не доступна';
-        alert(message);
+        // alert(message);
 }
+
+alert(message);
 
 
 
